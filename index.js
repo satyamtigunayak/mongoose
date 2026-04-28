57a4908ed945d43566caf8e238d5ec3de6598e54
satyam tigunayak <satyamtigunayak10@gmail.com>
2026-04-28 13:30:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const dbconnection=async()=>{
+    try{
+        await mongoose.connect("mongodb://localhost:27017/studb")
+        console.log('connect successfully');
+        
+    }
+    catch(error){
+    console.error('database connection error',error);
+    }
+}
+
+dbconnection()
+
+// define schema for students
+
+const studentschema= new mongoose.Schema({
+    name:String,
+    age:Number,
+    city:String
+})
+
+// create a model from studb connection
+
+const studentmodel= new mongoose.model("students",studentschema)
+
+const getstudent= async ()=>{
+  try{
+   const students= await studentmodel.find({})
+   console.log(students);
+   
+  } catch(error){
+    console.error('error fetching students',error);    
+  }
+}
+
+getstudent()
